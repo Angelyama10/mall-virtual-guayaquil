2d21fa7b905c7093f6fdc7c885ea67213a93f01d
Angel Yama <angelyama792@gmail.com>
2026-06-30 23:07:00 -0600

    feat: configuración de módulos de backend e integración con Prisma

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ProductsModule } from './modules/products/products.module';
+import { StoresModule } from './modules/stores/stores.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { PaymentsModule } from './modules/payments/payments.module';
+import { CartModule } from './modules/cart/cart.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, PrismaModule, AuthModule, ProductsModule, StoresModule, OrdersModule, PaymentsModule, CartModule],
   controllers: [AppController],
   providers: [AppService],
 })
